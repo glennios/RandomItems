ba24fb5f8460e9e2ecdaa213d1197549c048d0e9
Glenn Tu <glenn.tu@gmail.com>
2014-12-28 16:25:15 -0800

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Glenn on 12/28/14.
+//  Copyright (c) 2014 Glenn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
