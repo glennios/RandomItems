5f98c2f22ccec0915d789de6ad139c1dc804ee82
Glenn Tu <glenn.tu@gmail.com>
2014-12-30 14:47:21 -0800

    add BNCContainer

---
+++ RandomItems/BNRContainer.h
@@ -0,0 +1,13 @@
+//
+//  BNRContainer.h
+//  RandomItems
+//
+//  Created by Glenn on 12/30/14.
+//  Copyright (c) 2014 Glenn. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@interface BNRContainer : BNRItem
+
+@end
---
+++ RandomItems/BNRContainer.m
@@ -0,0 +1,13 @@
+//
+//  BNRContainer.m
+//  RandomItems
+//
+//  Created by Glenn on 12/30/14.
+//  Copyright (c) 2014 Glenn. All rights reserved.
+//
+
+#import "BNRContainer.h"
+
+@implementation BNRContainer
+
+@end
